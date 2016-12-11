0b07f27df2d729d0823dda7041d9b7c6a731136a
mjvalade <mjvalade@gmail.com>
2016-12-11 14:49:27 -0700

    set up storage functions

--- app/main.js
+++ app/main.js
@@ -3,20 +3,64 @@ const {
   BrowserWindow
 } = require('electron');
 
-const windows = new Set();
+const storage = require('electron-storage');
 
 app.on('ready', () => {
+  storageExists();
   createWindow();
 });
 
 const createWindow = () => {
   let win = new BrowserWindow({ show: false });
-  windows.add(win);
   win.loadURL(`file://${__dirname}/index.html`);
   win.once('ready-to-show', () => win.show());
   win.on('closed',() => {
-    windows.delete(win);
     win = null;
   });
   return win;
+};
+
+const data = { saved: [] };
+
+const storageExists = () => {
+  storage.isPathExists('savedRecipes.json')
+    .then(itDoes => {
+      if (!itDoes) {
+        storage.set('savedRecipes', data)
+        .then(() => {
+          console.log('Recipe successfully written to storage');
+        })
+        .catch(err => {
+          console.error(err);
+        });
+      }
+    });
+};
+
+const getRecipes = exports.getRecipes = () => {
+  storage.get('savedRecipes')
+  .then(data => {
+    console.log(data);
+  })
+  .catch(err => {
+    console.error(err);
+  });
+};
+
+const saveRecipe = exports.saveRecipe = (recipe) => {
+  storage.get('savedRecipes')
+    .then((data) => {
+      data.saved.unshift(recipe);
+      let updatedRecipes = { current: data.current, saved: data.saved };
+      storage.set('savedRecipes', updatedRecipes)
+        .then(() => {
+          console.log('Updated recipe list');
+        })
+        .catch(err => {
+          console.error(err);
+        });
+    })
+    .catch(err => {
+      console.error(err);
+    });
 };
--- app/renderer.js
+++ app/renderer.js
@@ -1,3 +1,5 @@
 const { ipcRenderer, remote } = require('electron');
 const mainProcess = remote.require('./main');
-const currentWindow = remote.getCurrentWindow();
+// const currentWindow = remote.getCurrentWindow();
+
+mainProcess.getRecipes();
