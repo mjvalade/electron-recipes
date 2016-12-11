b36f32a17a4f6bdfc812167fa487dce3e35d4957
mjvalade <mjvalade@gmail.com>
2016-12-11 15:15:05 -0700

    set up add-recipe inputs and variables and saveRecipe function

--- app/renderer.js
+++ app/renderer.js
@@ -2,4 +2,24 @@ const { ipcRenderer, remote } = require('electron');
 const mainProcess = remote.require('./main');
 // const currentWindow = remote.getCurrentWindow();
 
+const $name = $('.name-input');
+const $photo = $('.photo-upload');
+const $servings = $('.servings-input');
+const $time = $('.time-input');
+const $ingredients = $('.ingredients-input');
+const $directions = $('.directions-input');
+const $notes = $('.notes-input');
+const $addButton = $('.add-recipe-button');
+
 mainProcess.getRecipes();
+
+$addButton.on('click', () => {
+  let name = $name.val();
+  // let photo = $photo.val();
+  let servings = $servings.val();
+  let time = $time.val();
+  let ingredients = $ingredients.val();
+  let directions = $directions.val();
+  let notes = $notes.val();
+  mainProcess.saveRecipe({ name, servings, time, ingredients, directions, notes});
+});
