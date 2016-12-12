8c6fb0380b404d6bc8f742bf77654496b3ef9b87
Casey Cross <hellocaseycross@gmail.com>
2016-12-12 14:56:18 -0700

    Fixing the thing I did to upset MaryJane. Oops. She's better now.

--- app/renderer.js
+++ app/renderer.js
@@ -2,20 +2,25 @@ const { ipcRenderer, remote } = require('electron');
 const mainProcess = remote.require('./main');
 const currentWindow = remote.getCurrentWindow();
 
-const $name = $('.name-input');
-const $servings = $('.servings-input');
-const $time = $('.time-input');
-const $ingredients = $('.ingredients-input');
-const $directions = $('.directions-input');
-const $notes = $('.notes-input');
-const $saveButton = $('.save-recipe-button');
-const $seeAllButton = $('.see-all-button');
-const $homeButton = $('.home-button');
 const $addRecipeButton = $('.add-button');
+const $directions = $('#directions');
+const $homeButton = $('.home-button');
+const $ingredients = $('#ingredients');
+const $name = $('#name');
+const $newIngredient = $('.new-ingredient-button');
+const $notes = $('#notes');
+const $recipeCard = $('.recipe-card');
+const $recipeContainer = $('.recipe-list-container');
+const $saveButton = $('.save-recipe-button');
 const $searchButton = $('.search-button');
 const $searchInput = $('.search-input');
-const $recipeContainer = $('.recipe-list-container');
-const $recipeCard = $('.recipe-card');
+const $seeAllButton = $('.see-all-button');
+const $servings = $('#servings');
+const $time = $('#time');
+
+
+let inputCounter = 1;
+let limit = 10;
 
 mainProcess.getRecipes();
 
@@ -41,8 +46,29 @@ const renderRecipeCard = (data) => {
 };
 
 let pageNav = (page) => {
- currentWindow.loadURL(`file://${__dirname}/${page}`);
+  currentWindow.loadURL(`file://${__dirname}/${page}`);
 };
+
+
+// let addInput = (item) => {
+
+  // var newDiv = document.createElement('div');
+  // newDiv.innerHTML = "Ingredient " + (counter + 1) + "<input type='text' name='myIngredients[]'>";
+  // document.getElementById(divName).appendChild(newdiv);
+  // counter++;
+
+  // $('body').append('<p>This is the text in new element.<p>');
+  // let newListItem = `<label class="ingredients-label input-label" for="ingredients">
+  //     Ingredient + ${counter + 1} +:
+  //   </label>
+  //   <input id="ingredients" name="ingredientsList[]" type="text" class="input" />`
+  // $('.dynamicInput').append();
+  // newItem.val()
+// };
+
+$newIngredient.on('click', () => {
+
+});
 
 $saveButton.on('click', () => {
   let name = $name.val();
